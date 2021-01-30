b8ce4693e9b35d51c28e3419e1cdc8c6449fe734
klesniewski50 <63211654+klesniewski50@users.noreply.github.com>
2021-01-30 20:39:34 +0100

    Punkty 1.5.5 oraz 1.5.6

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -3,7 +3,12 @@ package edu.kis.vh.nursery;
 public class FIFORhymer extends defaultCountingOutRhymer {
 
 	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-	
+
+
+	/**
+	 *
+	 * metoda zwracająca zliczenia
+	 */
 	@Override
 	public int countOut() {
 		while (!callCheck())
